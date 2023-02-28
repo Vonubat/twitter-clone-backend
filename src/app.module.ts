7ab2dd5236edf0f34f6cee1f78417b2f392f12eb
Tabunov Egor <Vonubat@gmail.com>
2023-02-28 14:56:10 +0300

    feat: generate users controller, service, module refactor: del test files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UsersController } from './users/users.controller';
+import { UsersService } from './users/users.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UsersModule],
+  controllers: [UsersController],
+  providers: [UsersService],
 })
 export class AppModule {}
