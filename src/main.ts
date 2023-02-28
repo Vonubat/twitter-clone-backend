018dd609349a71ae387525bcd4580b7096e3d0db
Tabunov Egor <Vonubat@gmail.com>
2023-02-28 17:35:26 +0300

    feat: add dotenv environment variable files

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const PORT = process.env.PORT || 5000;
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  await app.listen(PORT, () => console.log(`Server started on port = ${PORT}`));
 }
 bootstrap();
