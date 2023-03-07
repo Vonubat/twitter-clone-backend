a52c510565625797b4754bd606aafd7117a6431b
Tabunov Egor <Vonubat@gmail.com>
2023-03-07 14:15:35 +0300

    feat: update port for server

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  const PORT = process.env.PORT || 5000;
+  const PORT = process.env.PORT || 3000;
 
   const app = await NestFactory.create(AppModule, {
     cors: { credentials: true, origin: true },
